56b94b521fc8bde0f430b7d40bc1d2356e957b11
alexeygold2077 <alexgold2077@yandex.ru>
2024-06-28 06:13:26 +0300

    use records for RequestData

--- src/main/java/com/alexeygold2077/api/proxyapi/DTO/RequestData.java
+++ src/main/java/com/alexeygold2077/api/proxyapi/DTO/RequestData.java
@@ -2,30 +2,8 @@ package com.alexeygold2077.api.proxyapi.DTO;
 
 import java.util.List;
 
-public class RequestData {
-    private String model;
-    private List<Message> messages;
-
-    public RequestData(String model, List<Message> messages) {
-        this.model = model;
-        this.messages = messages;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public List<Message> getMessages() {
-        return messages;
-    }
-
-    public void setMessages(List<Message> messages) {
-        this.messages = messages;
-    }
+public record RequestData(String model,
+                          List<Message> messages) {
 
     public void addMessage(String role, String message) {
         messages.add(
@@ -33,29 +11,6 @@ public class RequestData {
         );
     }
 
-    public static class Message {
-        private String role;
-        private String content;
-
-        public Message(String role, String content) {
-            this.role = role;
-            this.content = content;
-        }
-
-        public String getRole() {
-            return role;
-        }
-
-        public void setRole(String role) {
-            this.role = role;
-        }
-
-        public String getContent() {
-            return content;
-        }
-
-        public void setContent(String content) {
-            this.content = content;
-        }
-    }
+    public record Message(String role,
+                   String content) {}
 }
