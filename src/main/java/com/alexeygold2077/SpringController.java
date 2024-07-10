4abe51a238348570b58bda67935f5c984be7ca25
AlexeyGold2077 <alexgold2077@yandex.ru>
2024-07-10 21:23:56 +0300

    Changed api methods and mapping

--- src/main/java/com/alexeygold2077/SpringController.java
+++ src/main/java/com/alexeygold2077/SpringController.java
@@ -2,16 +2,11 @@ package com.alexeygold2077;
 
 import java.io.IOException;
 
-import com.alexeygold2077.api.DTO.ChatCompletionRequest;
 import com.alexeygold2077.api.Proxyapi;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.*;
-=======
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> refs/remotes/origin/master
 
 @RestController
 public class SpringController {
@@ -19,15 +14,9 @@ public class SpringController {
     @Autowired
     private Proxyapi ai;
 
-<<<<<<< HEAD
     @GetMapping("/sendmessageAsUser/gpt4")
     public String sendmessageAsUserGpt4(@RequestParam(value = "message") String message) throws IOException {
         return ai.getChatCompletionAsUser(message);
-=======
-    @GetMapping("/chatCompletionRequest")
-    public String greeting(@RequestBody ChatCompletionRequest chatCompletionRequest) throws IOException {
-        return ai.chatCompletionRequest(chatCompletionRequest);
->>>>>>> refs/remotes/origin/master
     }
 
     @GetMapping("/sendmessageAsUser/gpt-4o")
