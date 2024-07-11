9c0e7583da923c12bfda5ca92aa82bb603262d1a
AlexeyGold2077 <alexgold2077@yandex.ru>
2024-07-11 23:10:23 +0300

    working bot

--- src/main/java/com/alexeygold2077/ProxyapiApplication.java
+++ src/main/java/com/alexeygold2077/ProxyapiApplication.java
@@ -3,12 +3,10 @@ package com.alexeygold2077;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.alexeygold2077")
 public class ProxyapiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyapiApplication.class, args);
 	}
