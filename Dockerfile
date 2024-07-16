a709ef2dfed685f09fbd3819383c8914069034b8
AlexeyGold2077 <alexgold2077@yandex.ru>
2024-07-16 10:08:34 +0300

    new version, changed default port to 8080

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:17
 ARG JAR_FILE=target/*.jar
-COPY ./target/proxyapi-java-v2.1.0.jar app.jar
+COPY ./target/proxyapi-java-v2.3.2.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- build.sh
+++ build.sh
@@ -1,13 +1,32 @@
 # shellcheck disable=SC2162
+# shellcheck disable=SC2140
+# shellcheck disable=SC2016
+# shellcheck disable=SC2094
+# shellcheck disable=SC2094
 
 # set variables
-echo -ne "Enter release version: "
+echo -ne "Enter new release version: v"
 read RELEASE
+RELEASE="v$RELEASE"
+echo -ne "Enter previous release version: v"
+read PREV_RELEASE
+PREV_RELEASE="v$PREV_RELEASE"
+
+#edit configuration files
+sed "s|$PREV_RELEASE|$RELEASE|" pom.xml > temp_pom.xml && mv temp_pom.xml pom.xml
+sed "s|$PREV_RELEASE|$RELEASE|" Dockerfile > temp_Dockerfile && mv temp_Dockerfile Dockerfile
+
 # maven
 mvn clean
 mvn install
 mvn package
+
 # docker
-docker build -t proxyapi-java:latest -t proxyapi-java:"$RELEASE" .
+docker build --platform linux/amd64 -t proxyapi-java:latest -t proxyapi-java:"$RELEASE" .
 docker tag proxyapi-java:"$RELEASE" alexeyzolotarev2077/proxyapi-java:"$RELEASE"
 docker push alexeyzolotarev2077/proxyapi-java:"$RELEASE"
+
+#run
+
+#MacOS
+#docker run -d --platform linux/amd64 -p 8080:22870 alexeyzolotarev2077/proxyapi-java:latest
