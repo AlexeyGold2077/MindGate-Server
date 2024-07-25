8c1340d691f67b52ffecc888a22dfc87ffe2a17d
AlexeyGold2077 <alexgold2077@yandex.ru>
2024-07-25 14:26:22 +0300

    working /MindGate/hello-world/

--- src/main/java/com/alexeygold2077/SpringController.java
+++ src/main/java/com/alexeygold2077/SpringController.java
@@ -2,12 +2,15 @@ package com.alexeygold2077;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyController {
+@RequestMapping("/")
+public class SpringController {
 
     @GetMapping("/hello-world")
     public String getHelloWorld() {
         return "hello_world";
     }
+
 }
